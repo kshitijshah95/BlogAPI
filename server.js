7e5c77371070163450f005df788aa30774ec09cd
Kshitij Shah <kshitijshah95@gmail.com>
2020-08-02 21:30:45 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
+const app = express();
+
+mongoose.connect("mongodb://localhost/blog", {
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+});
+
+app.set("view engine", "ejs");
+
+// articleRouter appends the route within articles
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: "desc",
+  });
+  res.render("articles/index", { articles: articles });
+});
+
+app.use("/articles", articleRouter);
+
+app.listen(5000);
